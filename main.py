82aa0f3a78fa750c3100b773092a85396fd16278
FikriSatria11 <muhfikrisatria@gmail.com>
2021-02-14 21:00:51 +0700

    perbaiki bug

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL, MySQLdb
 import bcrypt
+# import mysql.connector
 
 app = Flask(__name__)
 
